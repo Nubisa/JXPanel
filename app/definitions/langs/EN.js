367757130e0596d5f3d4ca69fb360c010a37df3d
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2014-07-10 16:29:11 +0200

    login page

--- app/definitions/langs/EN.js
+++ app/definitions/langs/EN.js
@@ -13,7 +13,24 @@ exports.Labels = {
     "Control Panel":"Control Panel",
     "Account":"Account",
     "Home":"Home",
-    "MSBrowserOutOfDate": "Your browser is out of date, please update your browser by going to www.microsoft.com/download"
+    "MSBrowserOutOfDate": "Your browser is out of date, please update your browser by going to www.microsoft.com/download",
+
+    // login page
+    "SignIn" : "Sign In",
+    "StaySignedIn" : "Stay signed in",
+    "Username" : "User Name",
+    "UsernameEnter" : "Please enter your username",
+    "UsernameEnterRequired" : "Please enter valid username",
+
+    "Password" : "Password",
+    "PasswordForgot" : "Forgot password?",
+    "PasswordEnter" : "Enter your password",
+    "PasswordEnterRequired" : "Please enter your password",
+
+    "BlockLeftHeader" : "About SmartAdmin - Are you up to date?",
+    "BlockLeftBody" : "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.",
+    "BlockRightHeader" : "Not just your average template!",
+    "BlockRightBody" : "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi voluptatem accusantium!"
 };
 
 
