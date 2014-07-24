033f0908717a3880eee881c66cff43364b6fec44
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2014-07-24 16:38:00 +0200

    test for sqlite2 api

---
+++ app/db/test2.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Nubisa Inc. on 7/24/14.
+ */
+
+
+var sqlite2 = require("./sqlite2");
+
+var str = "test of the string 簎艜薤 佹侁刵 炟砏 輘, 斔櫅檷 藽轚酁 墏 犤繵 觓倎";
+
+
+
+sqlite2.UpdateDB(str, function(err) {
+    if (err) {
+        console.error("Cannot update.", err);
+    } else {
+        console.log("Updated OK.");
+
+
+        sqlite2.ReadDB(function(err2, str2) {
+            if (err2) {
+                console.error("Cannot read.", err2);
+                return;
+            }
+
+            if (str !== str2) {
+                console.error("String are not equal.");
+                console.error("str1", str);
+                console.error("str2", str2);
+                return;
+            }
+
+            console.log("Read OK.")
+        });
+    }
+});
